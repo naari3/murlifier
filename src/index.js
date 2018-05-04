a392a32ddc052d4f5c1c6cda94c975555495e72a
naari3 <naari.named@gmail.com>
2018-05-04 16:48:31 +0900

    implement new class Murlifier

--- src/index.js
+++ src/index.js
@@ -3,47 +3,56 @@ import eachCons from "each-cons";
 import path from "path";
 
 const kuromojiRoot = path.dirname(require.resolve("kuromoji/package"));
-const dicPath = path.join(kuromojiRoot, "dict") + path.sep;
 
-const builder = kuromoji.builder({ dicPath });
-
-const murlify = sentence => {
-  return new Promise((resolve, reject) => {
-    builder.build((err, tokenizer) => {
-      if (err) reject(err);
-
-      const tokens = tokenizer.tokenize(sentence);
-
-      resolve(tokensToMurSentence(tokensToMurTokens(tokens)));
-    });
-  });
-};
-
-const tokensToMurTokens = tokens => {
-  return eachCons(tokens, 2)
-    .concat([[tokens[tokens.length - 1]]])
-    .map(biTokens => {
-      if (biTokens.length === 1 || biTokens[1].pos === "記号") {
-        if (biTokens[0].pos === "助動詞" || biTokens[0].pos === "形容詞") {
-          biTokens[0].surface_form += "ゾ";
+class Murlifier {
+  constructor(dicPath) {
+    this.dicPath = dicPath || path.join(kuromojiRoot, "dict") + path.sep;
+    this.tokenizer = new Promise((resolve, rerject) => {
+      const builder = kuromoji.builder({ dicPath: this.dicPath });
+      builder.build((error, tokenizer) => {
+        if (error) {
+          return reject(errror);
         }
-        return biTokens[0];
-      } else {
-        return biTokens[0];
-      }
+        return resolve(tokenizer);
+      });
     });
-};
-
-const tokensToMurSentence = tokens => {
-  return tokens.map(token => murlizedWord(token)).join("");
-};
-
-const murlizedWord = token => {
-  if (token.pos_detail_1 === "終助詞") {
-    return "ゾ";
-  } else {
-    return token.surface_form;
   }
-};
 
-module.exports = murlify;
+  murlify(sentence) {
+    return new Promise((resolve, reject) => {
+      this.tokenizer.then(tokenizer => {
+        const tokens = tokenizer.tokenize(sentence);
+        resolve(this.tokensToMurSentence(this.tokensToMurTokens(tokens)));
+      });
+    });
+  }
+
+  tokensToMurTokens(tokens) {
+    return eachCons(tokens, 2)
+      .concat([[tokens[tokens.length - 1]]])
+      .map(biTokens => {
+        if (biTokens.length === 1 || biTokens[1].pos === "記号") {
+          if (biTokens[0].pos === "助動詞" || biTokens[0].pos === "形容詞") {
+            biTokens[0].surface_form += "ゾ";
+          }
+          return biTokens[0];
+        } else {
+          return biTokens[0];
+        }
+      });
+  }
+
+  tokensToMurSentence(tokens) {
+    return tokens.map(token => this.murlizedWord(token)).join("");
+  }
+
+  murlizedWord(token) {
+    if (token.pos_detail_1 === "終助詞") {
+      return "ゾ";
+    } else {
+      return token.surface_form;
+    }
+  }
+}
+
+module.exports = Murlifier;
